414b94b988710c148c8faded1f7e004ad7d5c08c
Michael Huttner <michael@mhuttner.com>
2025-11-16 17:11:57 +0100

    update contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <div className="py-20 bg-white">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 id="contact" className="text-5xl font-oswald text-rose-800 font-bold mb-8">Kontakt</h1>
-        <div className="flex items-center">
+        <div className="">
           <div>
             <a href="https://www.instagram.com/paulenzki_duo/"
               target="_blank" rel="noopener noreferrer"
