747583dde11aaf563fd7e6354465aef4786c6a8c
Michael Huttner <michael@mhuttner.com>
2025-11-16 17:30:50 +0100

    hero:

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,40 +3,43 @@ import { Link } from "react-router-dom";
 export default function Hero() {
   return (
     <div className="relative w-full h-screen overflow-hidden">
-      <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover">
+      <video autoPlay muted playsInline loop className="absolute top-0 left-0 w-full h-full object-cover">
         <source src="/videos/hero.mp4" type="video/mp4" />
         <source src="/videos/hero.webm" type="video/webm" />
       </video>
       <div className="relative z-10 flex flex-col items-center justify-center h-full bg-gray-500/40 text-center px-4">
-        <h1 className="text-5xl md:text-7xl font-eb-garamond  text-white font-bold mb-4">Paulenzki</h1>
+        <h1 className="text-5xl md:text-7xl font-eb-garamond capitalize text-white font-bold mb-4">Paulenzki</h1>
         <div className="border-b-8 border-rose-600 w-1/2 mb-4"></div>
         <p className="text-xl md:text-2xl text-white mb-8">Partnerakrobatik</p>
         <div>
           <ul className="inline-flex space-x-6 text-white">
-            <li>
-              <a href="https://www.instagram.com/paulenzki_duo/"
-                target="_blank" rel="noopener noreferrer"
-                className="hover:underline text-lg md:text-xl">
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/@paulenzki"
-                target="_blank" rel="noopener noreferrer"
-                className="text-white hover:underline text-lg md:text-xl">
-                YouTube
-              </a>
-            </li>
             <li>
               <Link to="#intro" className="text-white hover:underline text-lg md:text-xl">
                 Über uns
               </Link>
             </li>
             <li>
+              <Link to="#contact" className="text-white hover:underline text-lg md:text-xl">
+                Kontakt
+              </Link>
+            </li>
+            <li>
+              <Link to="#shows" className="text-white hover:underline text-lg md:text-xl">
+                Shows
+              </Link>
+            </li>
+            <li>
+              <Link to="#gallery" className="text-white hover:underline text-lg md:text-xl">
+                Gallerie
+              </Link>
+            </li>
+
+            <li>
               <Link to="#references" className="text-white hover:underline text-lg md:text-xl">
                 Referenzen
               </Link>
             </li>
+
           </ul>
         </div>
 
